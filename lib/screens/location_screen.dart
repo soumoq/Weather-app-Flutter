e6105a93a9e67d5441b7f3d39b2272aa474f1041
soumoq <soumojyotiganguly270@gmail.com>
2021-08-26 12:05:02 +0530

    Implement lat lon function in AndroidManifest.xml and IOS

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LocationScreen extends StatefulWidget {
   @override
@@ -7,6 +8,7 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
